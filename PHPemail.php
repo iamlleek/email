7c0f66c331527226b6ab1d8bc45cba7081bea7bb
iamlleek <66844593+iamlleek@users.noreply.github.com>
2020-11-18 15:05:04 -0500

    Add files via upload

---
+++ PHPemail.php
@@ -0,0 +1,33 @@
+<!-- Example from pages 143 - 144 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title>Email validator</title>
+</head>
+<body>
+	<h2 style = "color: purple; text-align: center;">Email Validator</h2>
+
+	<?php
+		$EmailAddresses = array("john.smith@php.test", "mary.smith.mail.php.example", "john.jones@php.invalid", "alan.smithee@test", "jsmith456@example", "jsmith456@test", "mjones@example", "mjones@example.net", "jane.a.doe@example.org");
+
+		function validateAddress($Address) {
+			// analyze the $Address string and check to see if it contains a "@" character and a '.' character
+			if(strpos($Address, '@') !== FALSE && strpos($Address, '.') !==FALSE){
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+
+		// global that loops through array and passes eachindividual email into validate Address() and checks the return boolean. If the return is FALSE we will write the address onto the page
+		foreach($EmailAddresses as $Address) {
+			if(validateAddress($Address) == FALSE){
+				echo "<p>The e-mail address <em>{$Address}</em>does not appear to be valid.</p>\n";
+			}
+		}
+	?>
+</body>
+</html>
